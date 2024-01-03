f5a80e0fbf7f76cf6ed1de0e895c50be7aa8cb11
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-01-03 13:41:05 -0500

    init shadcn and add Chakra Petch font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Chakra_Petch } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Chakra_Petch({ subsets: ['latin'], weight: "500"})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
