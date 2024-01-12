cc4148d806cbe184cf4a7866aea62ff34fc44b3f
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-01-12 16:07:35 -0500

    Update main tag style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between pt-16 pb-6">
+      <main className="flex min-h-screen flex-col items-center">
         <HomeSection />
         <ProjectsSection />
         <TechStackSection />
