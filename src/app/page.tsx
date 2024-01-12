5689a48ebf9922202a754480e8917f764d15e47f
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-01-12 01:11:44 -0500

    TechStackSection initialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header";
 import HomeSection from "@/components/home-section";
 import ProjectsSection from "@/components/projects-section";
+import TechStackSection from "@/components/tech-stack-section";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between pt-16 pb-6">
         <HomeSection />
         <ProjectsSection />
+        <TechStackSection />
       </main>
     </>
   );
---
+++ src/components/tech-stack-section.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function TechStackSection() {
+  return (
+    <>
+      {" "}
+      <h2
+        className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+        id="projects"
+      >
+        Tech Stack
+      </h2>
+    </>
+  );
+}
