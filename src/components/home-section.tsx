a69cf8a55e61b79ed7c7c9723056dc70f0834cd6
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-01-12 16:16:50 -0500

    Update HomeSection introduction

--- src/components/home-section.tsx
+++ src/components/home-section.tsx
@@ -7,7 +7,10 @@ import { LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 export default function HomeSection() {
   return (
-    <section className="w-full pt-12 md:pt-24 lg:pt-32 bg-secondary pb-12 md:pb-24 lg:pb-32" id="home">
+    <section
+      className="w-full pt-12 md:pt-24 lg:pt-32 bg-secondary pb-12 md:pb-24 lg:pb-32"
+      id="home"
+    >
       <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
         <div className="absolute top-20 right-2 mt-4 mr-4">
           <ModeToggle />
@@ -19,9 +22,10 @@ export default function HomeSection() {
               <span className=" text-green-500">Carlos Arévalo</span>
             </h1>
             <p className="mx-auto max-w-[700px] md:text-xl mt-6">
-              +3 years of experience as {" "}
-              <span className=" text-green-500">Software Developer</span>.
-              Passion for creating innovative solution with cutting-edge
+              +3 years of experience as{" "}
+              <span className=" text-green-500">Software Developer</span>. +300{" "}
+              <span className=" text-green-500">GitHub contributions</span> last
+              year. Passion for creating innovative solutions with cutting-edge
               technologies. As a freelancer, I have successfully delivered
               front-end and full-stack projects, consistently meeting
               clients&apos; requirements and deadlines. Staying abreast of the
@@ -32,19 +36,22 @@ export default function HomeSection() {
             </p>
             <div className="space-x-4 mt-6">
               <a href="https://github.com/carlosandresat" target="_blank">
-              <Button className="bg-green-500 hover:bg-green-600">
-                <Github />
-              </Button>
+                <Button className="bg-green-500 hover:bg-green-600">
+                  <Github />
+                </Button>
               </a>
-              <a href="https://www.linkedin.com/in/carlosandresat/" target="_blank">
-              <Button className="bg-green-500 hover:bg-green-600">
-                <Linkedin />
-              </Button>
+              <a
+                href="https://www.linkedin.com/in/carlosandresat/"
+                target="_blank"
+              >
+                <Button className="bg-green-500 hover:bg-green-600">
+                  <Linkedin />
+                </Button>
               </a>
               <a href="mailto:carlosandresat@hotmail.com" target="_blank">
-              <Button className="bg-green-500 hover:bg-green-600">
-                <Mail />
-              </Button>
+                <Button className="bg-green-500 hover:bg-green-600">
+                  <Mail />
+                </Button>
               </a>
             </div>
           </div>
