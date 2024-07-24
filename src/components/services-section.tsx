d1878f030bcd00927952ac5422638fcdfae5cfb4
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-07-23 23:49:53 -0500

    feat: update responsiveness

--- src/components/services-section.tsx
+++ src/components/services-section.tsx
@@ -7,7 +7,7 @@ import {
   CardFooter,
 } from "@/components/ui/card";
 import {
-    Award,
+  Award,
   BookOpenIcon,
   Code2,
   GithubIcon,
@@ -40,8 +40,8 @@ export default function ServicesSection() {
         My Services
       </h2>
       <div className="flex flex-col space-y-8 p-8 max-w-screen-xl  justify-center items-center">
-        <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row space-x-12 items-center justify-center">
-          <div className="flex space-x-8 md:space-x-12 items-center">
+        <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row md:space-x-12 items-center justify-center">
+          <div className="flex space-x-8 md:space-x-12 items-center w-full">
             <div className="flex flex-col">
               <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-right">
                 Tutorías Personalizadas
@@ -57,8 +57,8 @@ export default function ServicesSection() {
               <UserCheck className="w-8 h-8" />
             </div>
           </div>
-          <div className="flex space-x-8 md:space-x-12 items-center">
-          <div className="flex-shrink-0 w-24 h-24 flex justify-center items-center bg-secondary rounded-md">
+          <div className="flex space-x-8 md:space-x-12 items-center w-full">
+            <div className="flex-shrink-0 w-24 h-24 flex justify-center items-center bg-secondary rounded-md">
               <Award className="w-8 h-8" />
             </div>
 
@@ -76,13 +76,13 @@ export default function ServicesSection() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row space-x-12 items-center justify-center">
-          <div className="flex space-x-8 md:space-x-12 items-center">
+        <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row md:space-x-12 items-center justify-center">
+          <div className="flex space-x-8 md:space-x-12 items-center w-full">
             <div className="flex flex-col">
               <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-right">
                 Desarrollo de Aplicaciones Web a Medida
               </h4>
-              <p className="leading-7 text-right">
+              <p className="leading-7 mt-2 text-right">
                 Como desarrollador experimentado, ofrezco servicios de diseño y
                 desarrollo de aplicaciones web a medida. Desde la
                 conceptualización hasta el lanzamiento, manejo todos los
@@ -94,16 +94,16 @@ export default function ServicesSection() {
               <Code2 className="w-8 h-8" />
             </div>
           </div>
-          <div className="flex space-x-8 md:space-x-12 items-center">
-          <div className="flex-shrink-0 w-24 h-24 flex justify-center items-center bg-secondary md:bg-green-500 rounded-md">
+          <div className="flex space-x-8 md:space-x-12 items-center w-full">
+            <div className="flex-shrink-0 w-24 h-24 flex justify-center items-center bg-secondary md:bg-green-500 rounded-md">
               <Server className="w-8 h-8" />
             </div>
 
             <div className="flex flex-col">
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-right">
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
                 Desarrollo y Gestión de Bases de Datos
               </h4>
-              <p className="leading-7 mt-2 text-right">
+              <p className="leading-7 mt-2">
                 Especializado en el diseño y manejo de bases de datos, ofrezco
                 soluciones que garantizan la integridad, el rendimiento y la
                 escalabilidad de los datos. Perfecto para negocios que requieren
