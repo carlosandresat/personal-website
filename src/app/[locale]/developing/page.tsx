ad39d070808431b276a2e5f550dbcedad69a890e
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-07-25 21:35:05 -0500

    feat: add developing page

---
+++ src/app/[locale]/developing/page.tsx
@@ -0,0 +1,10 @@
+export default function Courses() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pt-24 lg:pt-32 px-8 text-center">
+        Desarrollo de Aplicaciones Web
+      </h1>
+      <p className="text-xl text-muted-foreground mt-6">Próximamente</p>
+    </main>
+  );
+}
