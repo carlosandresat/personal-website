7e1850a9d84a1a313cac49fbcc3769a658052353
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-07-25 21:41:33 -0500

    feat: iniatialize translation to developing page

--- src/app/[locale]/developing/page.tsx
+++ src/app/[locale]/developing/page.tsx
@@ -1,10 +1,14 @@
+import { useTranslations } from "next-intl";
+
 export default function Courses() {
+  const t = useTranslations("Developing");
+
   return (
     <main className="flex min-h-screen flex-col items-center">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pt-24 lg:pt-32 px-8 text-center">
-        Desarrollo de Aplicaciones Web
+        {t("title")}
       </h1>
-      <p className="text-xl text-muted-foreground mt-6">Próximamente</p>
+      <p className="text-xl text-muted-foreground mt-6">{t("message")}</p>
     </main>
   );
 }
