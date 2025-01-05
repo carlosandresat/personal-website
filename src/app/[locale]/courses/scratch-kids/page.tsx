2b33d0bbb15b00978c0673ddc28629511ddf96b9
Carlos Arevalo <carlosandresat@hotmail.com>
2025-01-04 23:47:25 -0500

    feat: add DiscountsDialog component for ScratchKids course page

--- src/app/[locale]/courses/scratch-kids/page.tsx
+++ src/app/[locale]/courses/scratch-kids/page.tsx
@@ -5,6 +5,7 @@ import { useTranslations } from 'next-intl';
 import { Dialog, DialogContent, DialogHeader, DialogTrigger, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import type { Metadata } from 'next';
+import { DiscountsDialog } from "@/components/discounts-dialog";
 
 type Props = {
   params: { locale: string }
@@ -212,55 +213,7 @@ export default function ScratchKids() {
                 </h4>
                 <p className="leading-4 text-center">{t('investment')}</p>
               </div>
-              <Dialog>
-                <DialogTrigger asChild><Button><BadgePercent className="mr-2" />{t('discounts')}</Button></DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>{t('discounts')}</DialogTitle>
-                    <DialogDescription>{t('discountsDescription')}</DialogDescription>
-                  </DialogHeader>
-                  <div className="flex flex-col">
-                    <div className="flex justify-between">
-                      <p className="leading-7">
-                        2 {t('students')}
-                      </p>
-                      <p className="leading-7">
-                        -20%
-                      </p>
-                      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                        $80
-                      </h4>
-                    </div>
-                    <div className="flex justify-between">
-                      <p className="leading-7">
-                        3 {t('students')}
-                      </p>
-                      <p className="leading-7">
-                        -30%
-                      </p>
-                      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                        $70
-                      </h4>
-                    </div>
-                    <div className="flex justify-between">
-                      <p className="leading-7">
-                        4 {t('students')}
-                      </p>
-                      <p className="leading-7">
-                        -40%
-                      </p>
-                      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                        $60
-                      </h4>
-                    </div>
-                  </div>
-                  <DialogFooter>
-                    <p className="text-sm text-muted-foreground">
-                    {t('discountsNote')}
-                    </p>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
+              <DiscountsDialog />
             </div>
           </div>
         </div>
