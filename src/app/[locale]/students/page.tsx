61a401a8fd61a1b6576f4e2947668a96091542e1
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-07 21:38:43 -0500

    fix: adjust paragraph styling on students page for better readability

--- src/app/[locale]/students/page.tsx
+++ src/app/[locale]/students/page.tsx
@@ -17,7 +17,7 @@ export default function Students() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pt-24 lg:pt-32 px-8 text-center">
         {t("title")}
       </h1>
-      <p className="text-xl text-muted-foreground p-8 max-w-screen-xl">
+      <p className="text-base text-muted-foreground px-8 pt-4 max-w-screen-xl">
         {t("message")}
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-screen-xl p-8 gap-8">
