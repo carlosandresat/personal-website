b14b77cb2341104f390b6e4b5fd9d9898aa9547e
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-11 17:24:56 -0500

    fix: remove unnecessary z-index from header elements for improved layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,14 +6,14 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Header() {
   return (
-    <header className="px-4 lg:px-6 h-16 flex items-center border-b fixed top-0 w-full bg-background z-10 justify-between">
+    <header className="px-4 lg:px-6 h-16 flex items-center border-b fixed top-0 w-full bg-background justify-between">
       <Link className="flex items-center justify-center" href="/">
         <Image alt="Logo" height="42" src="/logo.png" width="42" />
       </Link>
       <div className="md:fixed w-full left-0 flex justify-end md:justify-center">
         <Navbar />
       </div>
-      <div className="md:flex space-x-2 hidden z-10">
+      <div className="md:flex space-x-2 hidden">
         <LanguageSelect />
         <ModeToggle />
       </div>
