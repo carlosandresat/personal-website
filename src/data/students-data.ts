0576cbe08f5b2249e2fecff5bb1d6ec6c2221afe
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-11 17:05:19 -0500

    feat: add additional student information and course details in both English and Spanish

--- src/data/students-data.ts
+++ src/data/students-data.ts
@@ -3,19 +3,91 @@ export const studentsData = [
     code: "carlos-arevalo",
     name: "Carlos Arévalo",
     photoUrl: "/carlos.arevalo.jpg",
+    email: "carlosandresat@hotmail.com",
+    courses: [
+      {
+        code: "BasicsPython",
+        completed: "20-12-2024",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "Matplotlib",
+        completed: "12-01-2025",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "FrontI",
+        completed: "05-02-2025",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+    ],
   },
   {
     code: "ejemplo-estudiante",
     name: "Ejemplo Estudiante",
     photoUrl: "/carlos.arevalo.jpg",
+    email: "ejemploestudiante@hotmail.com",
+    courses: [
+      {
+        code: "FrontI",
+        completed: "20-12-2024",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "FrontII",
+        completed: "12-01-2025",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "FrontIII",
+        completed: "10-02-2025",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+    ],
   },
   {
     code: "otro-nombre",
     name: "Otro Nombre",
     photoUrl: "/carlos.arevalo.jpg",
+    email: "otronombre@hotmail.com",
+    courses: [
+      {
+        code: "Databases",
+        completed: "20-12-2023",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "BackExpress",
+        completed: "12-01-2024",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "BackNext",
+        completed: "10-02-2024",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+    ],
   },
   {
     code: "ultimo-estudiante",
     name: "Último Estudiante",
+    email: "ultimoejemplo@hotmail.com",
+    courses: [
+      {
+        code: "FrontI",
+        completed: "20-12-2024",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "FrontII",
+        completed: "12-01-2025",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+      {
+        code: "FrontIII",
+        completed: "10-02-2025",
+        projectUrl: "https://github.com/carlosandresat",
+      },
+    ],
   },
 ];
