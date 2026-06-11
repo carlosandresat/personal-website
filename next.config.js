094cf7cc0ca50b0de638f886f42f475fa2ca91f9
Carlos Arevalo <carlosandresat@hotmail.com>
2026-06-11 15:34:35 -0500

    fix(deps): configure packageManager and pnpm workspace for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const createNextIntlPlugin = require('next-intl/plugin');
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+    allowedDevOrigins: ['192.168.1.234'],
+};
+
 module.exports = withNextIntl(nextConfig);
